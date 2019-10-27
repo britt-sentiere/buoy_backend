0796a6741ea0f9a5747a442e6cf7b0b746f533fc
brittani <britt.sentiere@gmail.com>
2019-10-27 15:10:27 -0500

    updated associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  
+  has_secure_password
+  validates: :first_name, presence: true, uniqueness: true
+  validates: :last_name, presence: true, uniqueness: true
+
   belongs_to :student
   belongs_to :teacher
 
