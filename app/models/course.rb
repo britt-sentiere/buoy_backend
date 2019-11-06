8092de6909108b62ef16c831492694510553c4ac
brittani <britt.sentiere@gmail.com>
2019-11-06 15:46:41 -0600

    updated course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,7 +10,12 @@ class Course < ApplicationRecord
     beginning_of_day =  Time.now.beginning_of_day
     end_of_day = Time.now.end_of_day
 
-    participations_for_today = participations.joins(:help_requests).where("help_requests.created_at >= ? AND help_requests.created_at <= ?", beginning_of_day, end_of_day)
+    participations_for_today = participations.joins(:help_requests).where("help_requests.created_at >= ? AND help_requests.created_at <= ?", beginning_of_day, end_of_day).uniq
+
+    puts "*" * 50
+    puts "participations_for_today"
+    p participations_for_today.pluck(:id)
+    puts "*" * 50
 
     if participations_for_today.any?
       participations_for_today.select { |participation| participation.student.open_help_request }.sort_by { |participation| participation.student.open_help_request.created_at }
