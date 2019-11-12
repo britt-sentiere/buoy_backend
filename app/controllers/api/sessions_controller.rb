3cb91b49f1388bf0a026b1e2448f687c476870c4
brittani <britt.sentiere@gmail.com>
2019-11-12 10:46:27 -0600

    added courses to walter white

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -6,7 +6,7 @@ class Api::SessionsController < ApplicationController
         jwt = JWT.encode(
           {
             user_id: user.id, # the data to encode
-            exp: 24.hours.from_now.to_i # the expiration time
+            exp: 24.days.from_now.to_i # the expiration time
           },
           Rails.application.credentials.fetch(:secret_key_base), # the secret key
           'HS256' # the encryption algorithm
--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module BuoyBackend
     config.load_defaults 6.0
     config.autoloader = :classic
 
+    config.hosts << 'brittani.ngrok.io'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
